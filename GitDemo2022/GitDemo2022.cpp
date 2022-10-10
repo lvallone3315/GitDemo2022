619054d5a96a16dda82a500a387bee5b62fb6182
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2022-10-09 21:15:27 -0400

    3rd random change

--- GitDemo2022/GitDemo2022.cpp
+++ GitDemo2022/GitDemo2022.cpp
@@ -10,7 +10,8 @@ int main()
     std::cout << "I love pizza\n";
     std::cout << "I love pizza two\n";
     std::cout << "another random change\n";
-    std::cout << a 2nd random change\n";
+    std::cout << "a 2nd random change\n";
+    std::cout << "a third random change\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
